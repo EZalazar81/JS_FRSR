04a6a9d92c994ae8b6b1eddcb8f302392cdf7d25
Emilse Zalazar <emilsezalazar@gmail.com>
2022-09-26 17:31:14 -0300

    commit pagina web

---
+++ leccion01/01_01_holamundo.js
@@ -0,0 +1,10 @@
+// las variables en JS pueden cambiar su tipo de manera dinámica 
+
+var nombre = "Emilse"; //definimos variable - Identificador de tipo string
+nombre = "Emilse \nLorena\n"; //para que el nombre aparezca en diferentes lineas agregamos \n
+//inferencia de tipo var
+// nombre es el nombre de la variable y es de tipo string(lee el dato que contiene la variable)
+
+var apellido = "Zalazar";
+
+console.log(nombre + apellido); //para que aparezca el texto todo en la misma linea
---
+++ leccion01/HolaMundo.js
@@ -0,0 +1,5 @@
+
+var nombre = "Emilse"; 
+mensaje = "Hola Mundo desde JavaScript";
+
+console.log(mensaje);
